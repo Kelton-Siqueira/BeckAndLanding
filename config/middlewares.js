96f42d717adebe2b6948c61f127ca8050a087da8
Kelton-Siqueira <keltonsiiqueira@gmail.com>
2023-07-09 15:33:45 -0300

    first commit

---
+++ config/middlewares.js
@@ -0,0 +1,25 @@
+module.exports = [
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'dl.airtable'],
+          'media-src': ["'self'", 'date', 'blob', 'dl.airtable'],
+          upgradeInsecureRequests: null
+        },
+      },
+    },
+  },
+  'strapi::errors',
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
